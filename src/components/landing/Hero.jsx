785cfc9c097dce5b887698593e08e9b035d3f22d
Rabah Babaci <rabahbabaci@berkeley.edu>
2026-02-21 02:21:32 +0000

    File changes

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -202,8 +202,8 @@ export default function Hero() {
                             transition={{ duration: 0.6, delay: 0.1 }}
                         >
                             <h1 className="text-6xl lg:text-7xl font-black text-gray-900 leading-[1.15] mb-4 tracking-tight">
-                                Never Wait.<br />&nbsp;<br />
-                                Never Rush.<br />&nbsp;<br />
+                                Never Wait.<br /><span className="block h-2" />&nbsp;
+                                Never Rush.<br /><span className="block h-2" />&nbsp;
                                 <span className="text-indigo-600">Just Board.</span>
                                 {' '}
                                 <motion.span
